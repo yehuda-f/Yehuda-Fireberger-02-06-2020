c01acaf0b95bf0d250a14b20a43bb0cf2b302dab
Hod <fireberger@gmail.com>
2020-06-05 23:04:44 +0300

    Updates

---
+++ precache-manifest.45434563dc8078aefe23d3f2fb787c05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b27302d3d58948eac75a5ea8c492ddb",
+    "url": "/Yehuda-Fireberger-02-06-2020/frontend/index.html"
+  },
+  {
+    "revision": "e9d1509e287102158911",
+    "url": "/Yehuda-Fireberger-02-06-2020/frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "74bd67181fec284a1e7e",
+    "url": "/Yehuda-Fireberger-02-06-2020/frontend/static/js/2.1b7afd7d.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/Yehuda-Fireberger-02-06-2020/frontend/static/js/2.1b7afd7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9d1509e287102158911",
+    "url": "/Yehuda-Fireberger-02-06-2020/frontend/static/js/main.62dd817d.chunk.js"
+  },
+  {
+    "revision": "8ba6b709721088e75bf4",
+    "url": "/Yehuda-Fireberger-02-06-2020/frontend/static/js/runtime-main.3b95f7be.js"
+  }
+]);
